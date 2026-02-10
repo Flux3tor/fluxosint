2b0b2bca3d7adc2ba6d363113a7cc8388e0e4810
Flux3tor <tusharsinha199@gmail.com>
2026-02-10 19:47:12 +0530

    Render MORE FIXES

--- backend/main.py
+++ backend/main.py
@@ -7,12 +7,4 @@ app = FastAPI()
 
 app.include_router(target_router)
 app.include_router(leakguard_router)
-<<<<<<< HEAD
 app.include_router(jobs_router)
-=======
-
-def api_status():
-    return {"status": "FluxOSINT API online"}
-
-app.include_router(jobs_router)
->>>>>>> 0d5fb4fa3e95f7e1d21b53737c678fb9057b5338
