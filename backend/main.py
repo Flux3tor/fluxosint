6b796bb0e67062cf7661305c4a3c9eed7108a4ab
Flux3tor <tusharsinha199@gmail.com>
2026-02-15 18:50:10 +0530

    please be final bug

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,13 @@ from fastapi import FastAPI
 from backend.api.targets import router as target_router
 from backend.api.leakguard import router as leakguard_router
 from backend.api.jobs import router as jobs_router
+from backend.db import init_db
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    init_db()
 
 app.add_middleware(
     CORSMiddleware,
